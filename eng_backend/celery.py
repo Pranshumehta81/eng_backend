a9665bdffc9981e7be13ced794c2aa8f4182f3e6
Pranshumehta81 <72910904+Pranshumehta81@users.noreply.github.com>
2023-10-07 23:11:26 +0530

    celery.py

--- eng_backend/celery.py
+++ eng_backend/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eng_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TTM_backend.settings')
 
-app = Celery('eng_backend')
+app = Celery('TTM_backend')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
